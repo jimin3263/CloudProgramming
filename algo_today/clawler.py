6e8e1b02eea85139f4fbfb77977bcc3c11f01670
limjimin <dkdldsla0000@naver.com>
2022-06-09 05:07:24 +0900

    :sparkles: 알고리즘 문제 model 생성, 백준 문제 가져오기

---
+++ algo_today/clawler.py
@@ -0,0 +1,46 @@
+import requests, time, json, pandas as pd
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+
+import django
+django.setup()
+
+from algo_today.models import Problem
+
+def crawl(page):
+    url = "https://solved.ac/api/v3/search/problem"
+    querystring = {"query": " ", "page": f"{page}"}
+
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    temp = dict()
+    temp["item"] = json.loads(response.text).get("items")
+    for item in temp["item"]:
+        hash = int(item.get("problemId"))
+        title = item.get("titleKo")
+        tags = ""
+
+        info = item.get("tags")
+        length = len(info)
+        for idx, tag in enumerate(info):
+            temp_tag = tag.get("displayNames")
+            tags += temp_tag[1].get("short")
+
+            if idx == length - 1:
+                continue
+            tags += " "
+        print(hash)
+        #Problem.objects.create(title=title, tag=tags, number=hash)
+
+def main():
+    TodayProblem
+
+    # for i in range(1, 5):
+    #     print(f"crawling {i} page")
+    #     crawl(i)
+    #     time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
--- algo_today/models.py
+++ algo_today/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=500)
+    tag = models.CharField(max_length=500)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f'[{self.number}] {self.title}'
+
+    def get_absolute_url(self):
+        return f'/today/'
+
+class TodayProblem(models.Model):
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number}'
+
