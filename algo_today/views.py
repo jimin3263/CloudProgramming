fef8d3abf8a2ce0a873cd53aacd877ae42a81d84
limjimin <dkdldsla0000@naver.com>
2022-06-09 05:09:13 +0900

    :lipstick: 오늘의 알고리즘 뷰 생성

--- algo_today/views.py
+++ algo_today/views.py
@@ -1,8 +1,24 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
+from algo_today.models import Problem, TodayProblem
+from algo_today import sheduler
+
+
 def index(request):
+
+    #TodayProblem.objects.last().number
+    today_problem = Problem.objects.get(pk=1)
+    tags = today_problem.tag.split(" ")
+    context = {
+        'today_problem': today_problem,
+        'tags': tags
+    }
+
     return render(
         request,
-        'algo_today/algorithm_today.html'
+        'algo_today/algorithm_today.html',
+        context
     )
