abb0619a5c0b7342076df290a404b195946ab4c1
limjimin <dkdldsla0000@naver.com>
2022-06-19 00:47:09 +0900

    :bug: 로그인, 회원가입 예외 수정

--- algo_today/views.py
+++ algo_today/views.py
@@ -1,20 +1,23 @@
 import random
+import time
 
 from django.shortcuts import render
 
 # Create your views here.
+from algo_pages.models import Post
+from algo_today.clawler import crawl
 from algo_today.models import Problem, TodayProblem
-from algo_today import sheduler
 
 
 def index(request):
-
-    #TodayProblem.objects.last().number
-    today_problem = Problem.objects.get(pk=1)
+    #TodayProblem.objects.last()
+    today_problem = Problem.objects.get(pk=100)
     tags = today_problem.tag.split(" ")
+    count = Post.objects.filter(number=today_problem.number).count()
     context = {
         'today_problem': today_problem,
-        'tags': tags
+        'tags': tags,
+        'count': count
     }
 
     return render(
